c16a188ef703cc0cfbb53a7d716c2d8fe823874c
AlvinYC <chen.yongcheng@gmail.com>
2021-03-17 11:19:08 +0800

    fix TYPO tensorFlowTTS, new tts input text sample

--- e2e_tts.py
+++ e2e_tts.py
@@ -11,28 +11,28 @@ from tensorflow_tts.inference import AutoProcessor
 from synthesiser import do_synthesis
 import soundfile as sf
 
-tacotron2_config = AutoConfig.from_pretrained('TensorflowTTS/examples/tacotron2/conf/tacotron2.baker.v1.yaml')
+tacotron2_config = AutoConfig.from_pretrained('TensorFlowTTS/examples/tacotron2/conf/tacotron2.baker.v1.yaml')
 tacotron2 = TFAutoModel.from_pretrained(
     config=tacotron2_config,
     pretrained_path="content/tacotron2-100k.h5",
     name="tacotron2"
 )
 
-fastspeech2_config = AutoConfig.from_pretrained('TensorflowTTS/examples/fastspeech2/conf/fastspeech2.baker.v2.yaml')
+fastspeech2_config = AutoConfig.from_pretrained('TensorFlowTTS/examples/fastspeech2/conf/fastspeech2.baker.v2.yaml')
 fastspeech2 = TFAutoModel.from_pretrained(
     config=fastspeech2_config,
     pretrained_path="content/fastspeech2-200k.h5",
     name="fastspeech2"
 )
 
-mb_melgan_config = AutoConfig.from_pretrained('TensorflowTTS/examples/multiband_melgan/conf/multiband_melgan.baker.v1.yaml')
+mb_melgan_config = AutoConfig.from_pretrained('TensorFlowTTS/examples/multiband_melgan/conf/multiband_melgan.baker.v1.yaml')
 mb_melgan = TFAutoModel.from_pretrained(
     config=mb_melgan_config,
     pretrained_path="content/mb.melgan-920k.h5",
     name="mb_melgan"
 )
 
-input_text = "仁宝电脑位于台北市内湖区阳光街三八五号"
+input_text = "台中持续加码补助老旧机车淘汰换电动机车，今年度淘汰一至四期老旧机车换购电动机车"
 # setup window for tacotron2 if you want to try
 tacotron2.setup_window(win_front=5, win_back=5)
 mels, alignment_history, audios = do_synthesis(input_text, tacotron2, mb_melgan, "TACOTRON", "MB-MELGAN")
