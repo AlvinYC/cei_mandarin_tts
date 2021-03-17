2be2171a9fd0188e62027a513ff1a489ff1f4e3a
AlvinYC <chen.yongcheng@gmail.com>
2021-03-17 09:36:41 +0800

    folder utils_thisbuild include project_setup.sh only

---
+++ utils_thisbuild/project_setup.sh
@@ -0,0 +1,11 @@
+cd ~/
+#sh -c 'mv utils_thisbuild content'
+sh -c 'mkdir -p ~/workspace/content'
+cd ~/workspace/content
+sh -c 'ln -s ~/utils_thisbuild/model-100000.h5 tacotron2-100k.h5'
+sh -c 'ln -s ~/utils_thisbuild/model-200000.h5 fastspeech2-200k.h5'
+sh -c 'ln -s ~/utils_thisbuild/generator-920000.h5 mb.melgan-920k.h5'
+sh -c 'ln -s ~/utils_thisbuild/baker_mapper.json baker_mapper.json'
+# merge all id_rsa.pub into this container
+#sh -c 'cat /home/docker/utils_thisbuild/*pub > /home/docker/.ssh/authorized_keys'
+
