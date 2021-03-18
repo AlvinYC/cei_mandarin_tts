6a071e46eb0619fb9c75dceb1ec7b79b283b7a93
alvinyc <chen.yongcheng@gmail.com>
2021-03-18 10:18:39 +0800

    modulize e2e_tts, server just need to call e2e_tts subroutine

--- e2e_tts.py
+++ e2e_tts.py
@@ -15,58 +15,72 @@ from opencc import OpenCC
 import zhon.hanzi as hanzi
 import re
 from pycnnum import num2cn
+import uuid
 
 
-tacotron2_config = AutoConfig.from_pretrained('TensorFlowTTS/examples/tacotron2/conf/tacotron2.baker.v1.yaml')
-tacotron2 = TFAutoModel.from_pretrained(
-    config=tacotron2_config,
-    pretrained_path="content/tacotron2-100k.h5",
-    name="tacotron2"
-)
+d2c = lambda x: num2cn(int(x), numbering_type = 'high', alt_two = True, big = True, traditional= True) if x.isdigit() else x
+cc = OpenCC('t2s')
 
-fastspeech2_config = AutoConfig.from_pretrained('TensorFlowTTS/examples/fastspeech2/conf/fastspeech2.baker.v2.yaml')
-fastspeech2 = TFAutoModel.from_pretrained(
-    config=fastspeech2_config,
-    pretrained_path="content/fastspeech2-200k.h5",
-    name="fastspeech2"
-)
+def load_model():
+    tacotron2_config = AutoConfig.from_pretrained('TensorFlowTTS/examples/tacotron2/conf/tacotron2.baker.v1.yaml')
+    tacotron2 = TFAutoModel.from_pretrained(
+        config=tacotron2_config,
+        pretrained_path="content/tacotron2-100k.h5",
+        name="tacotron2"
+    )
 
-mb_melgan_config = AutoConfig.from_pretrained('TensorFlowTTS/examples/multiband_melgan/conf/multiband_melgan.baker.v1.yaml')
-mb_melgan = TFAutoModel.from_pretrained(
-    config=mb_melgan_config,
-    pretrained_path="content/mb.melgan-920k.h5",
-    name="mb_melgan"
-)
+    fastspeech2_config = AutoConfig.from_pretrained('TensorFlowTTS/examples/fastspeech2/conf/fastspeech2.baker.v2.yaml')
+    fastspeech2 = TFAutoModel.from_pretrained(
+        config=fastspeech2_config,
+        pretrained_path="content/fastspeech2-200k.h5",
+        name="fastspeech2"
+    )
+
+    mb_melgan_config = AutoConfig.from_pretrained('TensorFlowTTS/examples/multiband_melgan/conf/multiband_melgan.baker.v1.yaml')
+    mb_melgan = TFAutoModel.from_pretrained(
+        config=mb_melgan_config,
+        pretrained_path="content/mb.melgan-920k.h5",
+        name="mb_melgan"
+    )
+    return  tacotron2,fastspeech2,mb_melgan
 
 def sec2numpy(sec):
     return np.zeros(int(24000*sec))
 
-d2c = lambda x: num2cn(int(x), numbering_type = 'high', alt_two = True, big = True, traditional= True) if x.isdigit() else x
 
-cc = OpenCC('t2s')
+def e2e_tts(input_text,tacotron2,fastspeech2,mb_melgan):
+    uuidcode = uuid.uuid4().hex
 
-#input_text = "台中持续加码补助老旧机车淘汰换电动机车，今年度淘汰1至4期老旧机车换购电动机车"
-input_text = "台中持續加碼補助老舊機車汰換電動機車，今年度淘汰1至4期老舊機車換購電動機車"
-# digit to mandarin char
-char_digits = re.findall(r'[\u4e00-\u9fff]+|[\uFF01-\uFF5E]+|[0-9]+', input_text)
-char_char = list(map(lambda x: d2c(x), char_digits))
-input_text = ''.join(char_char)
-# conver trandition to simple char
-input_text =  cc.convert(input_text) 
-# sentence segmentation by punctuation
-input_list = re.split('['+hanzi.punctuation+']',input_text)
+    text = input_text
+    
+    #input_text = "台中持续加码补助老旧机车淘汰换电动机车，今年度淘汰1至4期老旧机车换购电动机车"
+    #input_text = "台中持續加碼補助老舊機車汰換電動機車，今年度淘汰1至4期老舊機車換購電動機車"
+    # digit to mandarin char
+    char_digits = re.findall(r'[\u4e00-\u9fff]+|[\uFF01-\uFF5E]+|[0-9]+', text)
+    char_char = list(map(lambda x: d2c(x), char_digits))
+    input_text = ''.join(char_char)
+    # conver trandition to simple char
+    input_text =  cc.convert(input_text) 
+    # sentence segmentation by punctuation
+    input_list = re.split('['+hanzi.punctuation+']',input_text)
 
-au1 = np.array([])
-au2 = np.array([])
-# setup window for tacotron2 if you want to try
-tacotron2.setup_window(win_front=5, win_back=5)
-for idx, txt in enumerate(input_list):
-    mels, alignment_history, audios = do_synthesis(txt, tacotron2, mb_melgan, "TACOTRON", "MB-MELGAN")
-    au1 = audios  if idx == 0 else np.concatenate((au1,sec2numpy(0.1), audios),axis=0)
-    mels, audios = do_synthesis(txt, fastspeech2, mb_melgan, "FASTSPEECH2", "MB-MELGAN")
-    au2 = audios  if idx == 0 else np.concatenate((au2,sec2numpy(0.1),audios),axis=0)
+    tacotron2.setup_window(win_front=5, win_back=5)
+    au1 = np.array([])
+    au2 = np.array([])
+    for idx, txt in enumerate(input_list):
+        mels, alignment_history, audios = do_synthesis(txt, tacotron2, mb_melgan, "TACOTRON", "MB-MELGAN")
+        au1 = audios  if idx == 0 else np.concatenate((au1,sec2numpy(0.1), audios),axis=0)
+        mels, audios = do_synthesis(txt, fastspeech2, mb_melgan, "FASTSPEECH2", "MB-MELGAN")
+        au2 = audios  if idx == 0 else np.concatenate((au2,sec2numpy(0.05),audios),axis=0)
+    
+    sf.write('./output/'+str(uuidcode)+ '_tacotron.wav', au1, 24000, 'PCM_24')
+    sf.write('./output/'+str(uuidcode)+ '_factspeech.wav', au2, 24000, 'PCM_24')
+        
+    return True
 
-sf.write('./output/tacotron.wav', au1, 24000, 'PCM_24')
-sf.write('./output/factspeech.wav', au2, 24000, 'PCM_24')
-
+if __name__=="__main__":
+    input_text = "台中持续加码补助老旧机车淘汰换电动机车，今年度淘汰一至四期老旧机车换购电动机车"
+    tacotron2,fastspeech2,mb_melgan = load_model()
+    e2e_tts(input_text, tacotron2,fastspeech2,mb_melgan)
+    print("TTS done")
 
--- server.py
+++ server.py
@@ -14,11 +14,7 @@ import soundfile as sf
 from flask import Flask, request
 import json
 import argparse
-import uuid
-from opencc import OpenCC
-import zhon.hanzi as hanzi
-import re
-from  pycnnum import num2cn
+from e2e_tts import load_model,e2e_tts
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--address',
@@ -35,36 +31,10 @@ parser.add_argument('--port',
 args = parser.parse_args()
 app = Flask(__name__)
 
-tacotron2_config = AutoConfig.from_pretrained('TensorFlowTTS/examples/tacotron2/conf/tacotron2.baker.v1.yaml')
-tacotron2 = TFAutoModel.from_pretrained(
-    config=tacotron2_config,
-    pretrained_path="content/tacotron2-100k.h5",
-    name="tacotron2"
-)
-
-fastspeech2_config = AutoConfig.from_pretrained('TensorFlowTTS/examples/fastspeech2/conf/fastspeech2.baker.v2.yaml')
-fastspeech2 = TFAutoModel.from_pretrained(
-    config=fastspeech2_config,
-    pretrained_path="content/fastspeech2-200k.h5",
-    name="fastspeech2"
-)
-
-mb_melgan_config = AutoConfig.from_pretrained('TensorFlowTTS/examples/multiband_melgan/conf/multiband_melgan.baker.v1.yaml')
-mb_melgan = TFAutoModel.from_pretrained(
-    config=mb_melgan_config,
-    pretrained_path="content/mb.melgan-920k.h5",
-    name="mb_melgan"
-)
-
-cc = OpenCC('t2s')
-def sec2numpy(sec):
-    return np.zeros(int(24000*sec))
-d2c = lambda x: num2cn(int(x), numbering_type = 'high', alt_two = True, big = True, traditional= True) if x.isdigit() else x
+tacotron2,fastspeech2,mb_melgan = load_model()
 
 @app.route('/tts', methods=['GET', 'POST'])
 def do_tts():
-    #logger.info('Get request')
-    uuidcode = uuid.uuid4().hex
     if request.method == 'POST':
         if not request.is_json:
             return 'Only json format is supported'
@@ -72,35 +42,11 @@ def do_tts():
         if 'text' not in data:
             return 'Your POST data must contain \'text\' key'
         text = data['text']
-        
-        #input_text = "台中持续加码补助老旧机车淘汰换电动机车，今年度淘汰1至4期老旧机车换购电动机车"
-        #input_text = "台中持續加碼補助老舊機車汰換電動機車，今年度淘汰1至4期老舊機車換購電動機車"
-        # digit to mandarin char
-        char_digits = re.findall(r'[\u4e00-\u9fff]+|[\uFF01-\uFF5E]+|[0-9]+', text)
-        char_char = list(map(lambda x: d2c(x), char_digits))
-        input_text = ''.join(char_char)
-        # conver trandition to simple char
-        input_text =  cc.convert(input_text) 
-        # sentence segmentation by punctuation
-        input_list = re.split('['+hanzi.punctuation+']',input_text)
-
-        tacotron2.setup_window(win_front=5, win_back=5)
-        au1 = np.array([])
-        au2 = np.array([])
-        for idx, txt in enumerate(input_list):
-            mels, alignment_history, audios = do_synthesis(txt, tacotron2, mb_melgan, "TACOTRON", "MB-MELGAN")
-            au1 = audios  if idx == 0 else np.concatenate((au1,sec2numpy(0.1), audios),axis=0)
-            mels, audios = do_synthesis(txt, fastspeech2, mb_melgan, "FASTSPEECH2", "MB-MELGAN")
-            au2 = audios  if idx == 0 else np.concatenate((au2,sec2numpy(0.1),audios),axis=0)
-        
-        sf.write('./output/'+str(uuidcode)+ '_tacotron.wav', au1, 24000, 'PCM_24')
-        sf.write('./output/'+str(uuidcode)+ '_factspeech.wav', au2, 24000, 'PCM_24')
-            
+        e2e_tts(text, tacotron2,fastspeech2,mb_melgan)        
         return "TTS done\n"
     else:
         return 'GET method is not supported here.'
 
 if __name__ == "__main__":
-    #logger.info(f'Listening to {args.address} at {args.port}...')
     app.run(args.address, args.port)
 
